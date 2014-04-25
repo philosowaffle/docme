32cfdcf7e92ada90a30b1446994bdf04bf151a97
philosowaffle <baileyb622@gmail.com>
2014-04-25 09:02:49 -0500

    Create testJS.js

---
+++ test/testJS.js
@@ -0,0 +1,29 @@
+/*
+    +[title]: myFunction
+    +[description]: a basic javascript function
+    +[input]: {
+            -[var]: firstVar-> the first var we take,
+            -[var]: secondVar-> the second var we take
+    }
+    +[output]: returns a dinosaur
+    +[example]: {
+            -[code]: {
+                  <script>
+                      var example = myfunction(firstVar, secondVar);
+                  </script>
+                  
+            }
+    }
+    +[dependencies]: {
+            -[dependson]: jquery
+    }
+    +[compatability]: {
+            -[compat]: chrome,
+            -[compat]: firefox
+    }
+
+*/
+
+function myFunction(firstVar, secondVar){
+  return "a dinosaur";
+}
